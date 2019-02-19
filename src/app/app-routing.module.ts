e9510e321c4dba60377dac6e8b2cd50badf5c0ad
Justin <justinrae13@gmail.com>
2019-02-19 22:49:27 +0100

    Update app-routing.module.ts
    Register route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'tabs/offers', loadChildren: '../pages/offers/offers.module#OffersPageModule' },
   { path: 'tabsadmin/users', loadChildren: '../pages/users/users.module#UsersPageModule' },
   { path: 'tabsproprio/bar', loadChildren: '../pages/bar/bar.module#BarPageModule' },
+  { path: 'register', loadChildren: '../pages/register/register.module#RegisterPageModule' },
 
 ];
 

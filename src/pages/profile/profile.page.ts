6de28b86aded55e4516e02f570651917536d602a
Justin <justinrae13@gmail.com>
2019-02-26 15:53:52 +0100

    Update profile.page.ts

--- src/pages/profile/profile.page.ts
+++ src/pages/profile/profile.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  sessionId : any;
+  loggedUser : any = {};
+  baseURI = 'https://macfi.ch/serveur/';
 
-  ngOnInit() {
+  constructor(public navCtrl : NavController, private storage: Storage, private http : HttpClient, private loadingCtrl : LoadingController) { 
+    this.storage.get('SessionIdKey').then((val) => {
+      this.sessionId = val;
+    });
+
+    this.loadData();
+  }
+
+  ngOnInit() {}
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Hellooo',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log('Loading dismissed!');
+  }
+
+  loadData(){
+    let headers 	: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+        options 	: any		= { "key" : "getUsers"},
+        url       : any   = this.baseURI + 'aksi.php';
+
+    this.http.post(url, JSON.stringify(options), headers).subscribe((data : any) =>
+    {
+      this.loggedUser = data.filter(userData => userData.INT_ID == this.sessionId);
+      console.log(this.loggedUser);
+    },
+    (error : any) =>
+    {
+      console.dir(error);
+    });
+  }
+
+  logout(){
+    this.storage.remove('SessionIdKey');
+    this.storage.remove('SessionInKey');
+    this.storage.remove('SessionRoleKey');
+    this.navCtrl.navigateBack('login');
   }
 
 }

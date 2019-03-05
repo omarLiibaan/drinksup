1daddbc59e62b34be168cb90a5b6cae60a89da2d
Justin <justinrae13@gmail.com>
2019-03-05 18:53:01 +0100

    Update bars.page.ts

--- src/pages/bars/bars.page.ts
+++ src/pages/bars/bars.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-bars',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bars.page.scss'],
 })
 export class BarsPage implements OnInit {
+  baseURI = 'https://macfi.ch/serveur/aksi.php';
+  uplPhotoURI = "https://www.macfi.ch/serveur/barphotos/";
+  items : Array<any> = [];
+  // barNOMS :Array<any> = [];
+  // imgUrl : Array<any> = [];
 
-  constructor() { }
+  constructor(private http : HttpClient, private storage : Storage) { 
+    this.loadBar();
+  }
 
   ngOnInit() {
+  
+  }
+
+  loadBar() : void{
+    let headers 	: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+        options 	: any		= { "key" : "fetchActiveBar"},
+        url       : any      	= this.baseURI;
+
+    this.http.post(url, JSON.stringify(options), headers).subscribe((data : any) =>
+    {
+        console.log(data);
+        this.items = data;
+        // for(let i=0; i<this.items.length;i++){
+        //    this.barNOMS.push(this.items[i].ENT_NOM);
+        // }   
+    },
+    (error : any) =>
+    {
+      console.log(error);
+    });
   }
 
 }

5e62901491aec58ac9c4700999a8a3b2bbce94c4
Justin <justinrae13@gmail.com>
2019-03-08 15:01:35 +0100

    Create bar-user.module.ts

---
+++ src/pages/bar-user/bar-user.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { BarUserPage } from './bar-user.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BarUserPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [BarUserPage]
+})
+export class BarUserPageModule {}

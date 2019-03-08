f8d0c39b9ccac8749bb6e800df453247ba52d2b1
Justin <justinrae13@gmail.com>
2019-03-08 15:01:57 +0100

    Create loadingpage.module.ts

---
+++ src/pages/loadingpage/loadingpage.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoadingpagePage } from './loadingpage.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoadingpagePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoadingpagePage]
+})
+export class LoadingpagePageModule {}

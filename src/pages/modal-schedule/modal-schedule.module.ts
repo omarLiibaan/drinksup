3e9d79768fc56eade1f11e03b858b8722e022852
Justin <justinrae13@gmail.com>
2019-03-08 15:02:06 +0100

    Create modal-schedule.module.ts

---
+++ src/pages/modal-schedule/modal-schedule.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ModalSchedulePage } from './modal-schedule.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ModalSchedulePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ModalSchedulePage]
+})
+export class ModalSchedulePageModule {}

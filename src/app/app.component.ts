0c92951b55e5d051defb83efd52b0a9781b8ec06
Justin <justinrae13@gmail.com>
2019-03-12 16:54:50 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 
 @Component({
@@ -9,12 +11,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  @ViewChild('content') nav: NavController;
+  userSessionRole : string;
+  roleUser = 'user';
+  roleAdmin = 'admin';
+  roleProprio = 'proprio';
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private storage: Storage,
+    private router: Router,
+    private navCtrl : NavController
   ) {
     this.initializeApp();
   }
@@ -28,6 +36,27 @@ export class AppComponent {
         this.splashScreen.hide();
       }, 1800);
 
+      this.storage.get('SessionInKey').then((val) => {
+        this.storage.get('SessionRoleKey').then((valRole) => {
+            this.userSessionRole = valRole;
+
+            if(val=='Yes' && this.userSessionRole == this.roleAdmin){
+                this.navCtrl.navigateRoot('/tabsadmin/users');
+                // this.router.navigateByUrl('/tabsadmin/users');
+            }else if(val=='Yes' && this.userSessionRole == this.roleProprio){
+                this.navCtrl.navigateRoot('/tabsproprio/bar');
+                // this.router.navigateByUrl('/tabsproprio/bar');
+            }else if(val=='Yes' && this.userSessionRole == this.roleUser){
+                this.navCtrl.navigateRoot('/tabs/offers');
+                // this.router.navigateByUrl('/tabs/offers');
+            }else{
+                return null;
+            }
+        });
+      });
+
+
     });
+
   }
 }

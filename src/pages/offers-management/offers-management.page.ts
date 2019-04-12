d5ef80a417f7c0fadfe10946f084889f5d146ac4
omarliibaan <omar.liibaan@gmail.com>
2019-04-12 18:38:27 +0200

    Calendrier ajouter offre + delete

--- src/pages/offers-management/offers-management.page.ts
+++ src/pages/offers-management/offers-management.page.ts
@@ -3,19 +3,23 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AlertController, IonItemSliding, ModalController, ToastController} from '@ionic/angular';
 import {OffersAddbarPage} from '../offers-addbar/offers-addbar.page';
 import {CalendarComponent} from 'ionic2-calendar/calendar';
-import {formatDate} from "@angular/common";
+import {DatePipe, formatDate} from '@angular/common';
+
 
 @Component({
   selector: 'app-offers-management',
   templateUrl: './offers-management.page.html',
   styleUrls: ['./offers-management.page.scss'],
+  providers: [DatePipe]
 })
 export class OffersManagementPage implements OnInit {
     event = {
         title: '',
-        desc: '',
-        startTime: '',
-        endTime: '',
+        description: '',
+        startTime: new Date().toISOString(),
+        endTime: new Date().toISOString(),
+        entreprise: '',
+        idOffre: '',
         allDay: false
     };
     minDate = new Date().toISOString();
@@ -25,28 +29,90 @@ export class OffersManagementPage implements OnInit {
         currentDate: new Date()
     }
     viewTitle = '';
+    users = [];
+    usersFilter = [];
+    listeOffres = [];
+    public baseURI = 'https://macfi.ch/serveur/';
+    offres = {
+        title: '',
+        description: '',
+        startTime: new Date(),
+        endTime: new Date(),
+        entreprise: '',
+        idOffre: '',
+        allDay: false
+    };
+    noEventsLabel = '';
     @ViewChild(CalendarComponent) myCal: CalendarComponent;
-    constructor(public alertCtrl: AlertController, @Inject(LOCALE_ID)private locale: string) { }
+    constructor(public dp: DatePipe, public alertCtrl: AlertController, @Inject(LOCALE_ID)private locale: string, public http: HttpClient, public toastCtrl: ToastController, public alertController: AlertController) { }
     ngOnInit() {
+        // this.ionViewWillEnter();
+        this.myCal.loadEvents();
+        this.noEventsLabel = 'Pas d\'offre ';
+    }
+
+    public ionViewWillEnter(): void {
         this.resetEvents();
+        this.eventSource = [];
+        this.getOffres();
+        this.getProprio();
+        this.myCal.loadEvents();
     }
 
     resetEvents() {
         this.event = {
             title: '',
-            desc: '',
+            description: '',
             startTime: new Date().toISOString(),
             endTime: new Date().toISOString(),
+            entreprise: '',
+            idOffre: '',
             allDay: false
         };
+    }
+
+    public getProprio() {
+        const headers: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+            options: any		= { 'key' : 'getProprioValide'},
+            url: any      	= this.baseURI + 'aksi.php';
+        this.http.post(url, JSON.stringify(options), headers).subscribe((data: any) => {
+            this.users = data;
+            this.usersFilter = this.users;
+        });
+
+    }
+    public getOffres() {
+        const headers: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+            options: any		= { 'key' : 'getOffres'},
+            url: any      	= this.baseURI + 'aksi.php';
+        this.http.post(url, JSON.stringify(options), headers).subscribe((data: any) => {
+            this.listeOffres = data;
+            for (let i = 0; i < this.listeOffres.length; i++) {
+                this.offres = {
+                    title: this.listeOffres[i].title.toString(),
+                    description: this.listeOffres[i].description.toString(),
+                    startTime: new Date(this.listeOffres[i].startTime),
+                    endTime: new Date(this.listeOffres[i].startTime),
+                    entreprise: this.listeOffres[i].entreprise,
+                    idOffre: this.listeOffres[i].idOffre,
+                    allDay: false
+                }
+                this.eventSource.push(this.offres);
+            }
+
+            this.resetEvents();
+            this.myCal.loadEvents();
+        });
     }
 
     addEvent() {
         const eventCopy = {
             title: this.event.title,
-            desc: this.event.desc,
+            description: this.event.description,
             startTime: new Date(this.event.startTime),
             endTime: new Date(this.event.endTime),
+            entreprise: this.event.entreprise,
+            idOffre: this.event.idOffre,
             allDay: this.event.allDay
         }
         if (eventCopy.allDay) {
@@ -55,10 +121,27 @@ export class OffersManagementPage implements OnInit {
            eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
            eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1 ));
         }
-
+        const dateDebut = this.dp.transform(eventCopy.startTime, 'yyyy-MM-dd HH:mm');
+        const dateFin = this.dp.transform(eventCopy.endTime, 'yyyy-MM-dd  HH:mm');
+        this.addOffer(eventCopy.description, dateDebut, dateFin, eventCopy.title);
         this.eventSource.push(eventCopy);
         this.myCal.loadEvents();
         this.resetEvents();
+    }
+
+    addOffer(description: String, dateDebut: String, dateFin: String, idEnt: String) {
+        const headers: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+            options: any		= { 'key' : 'insertOffers', 'description': description, 'dateDebut': dateDebut, 'dateFin': dateFin, 'idEnt': idEnt},
+            url: any      	= this.baseURI + 'aksi.php';
+
+        this.http.post(url, JSON.stringify(options), headers).subscribe((data: any) => {
+                this.sendNotification('L\'ajout de l\'offre a bien été pris en compte!');
+                this.myCal.loadEvents();
+            },
+            (error: any) => {
+                console.log(error);
+                this.sendNotification('Erreur!');
+            });
     }
     changeMode(mode) {
         this.calendar.mode = mode;
@@ -76,12 +159,13 @@ export class OffersManagementPage implements OnInit {
     today() {
         this.calendar.currentDate = new Date();
     }
-    async onEventSelected(event) {
-        const start = formatDate(event.startTime, 'medium', this.locale);
-        const end = formatDate(event.endTime, 'medium', this.locale);
+    async eventSelected(event) {
+        const start = formatDate(event.startTime, 'dd/MM/yyyy HH:mm', this.locale);
+        const end = formatDate(event.endTime, 'dd/MM/yyyy HH:mm', this.locale);
+
         const alert = await this.alertCtrl.create({
-            header: event.title,
-            subHeader: event.desc,
+            header: event.entreprise,
+            subHeader: event.description,
             message: 'De : ' + start + '<br><br> à ' + end,
             buttons: ['OK']
         });
@@ -96,9 +180,59 @@ export class OffersManagementPage implements OnInit {
         this.event.startTime = selected.toISOString();
         selected.setHours(selected.getHours() + 1);
         this.event.endTime = (selected.toISOString());
-
     }
 
+    async delete(slidingItem: IonItemSliding, id, nom) {
+        await slidingItem.close();
+        console.log('id de l\'offre ' + id);
+        this.presentAlert(id, nom);
+    }
+    deleteOffre(id: number) {
+        const headers: any		= new HttpHeaders({ 'Content-Type': 'application/json' }),
+            options: any		= { 'key' : 'deleteOffre', 'id': id},
+            url: any      	= this.baseURI + 'aksi.php';
+
+        this.http.post(url, JSON.stringify(options), headers).subscribe((data: any) => {
+                this.sendNotification('Votre suppresion a bien été pris en compte !');
+                console.log('ouais bien');
+            },
+            (error: any) => {
+                console.log(error);
+                this.sendNotification('Erreur!');
+            });
+    }
+    async presentAlert(id, nom) {
+        const alert = await this.alertController.create({
+            header: 'Êtes vous sûr de supprimer l\'offre de ' + nom,
+            buttons: [
+                {
+                    text: 'Non',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                }, {
+                    text: 'Oui',
+                    handler: () => {
+                        this.deleteOffre(id);
+                        this.ionViewWillEnter();
+                        console.log('Confirm Okay');
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+    async sendNotification(msg: string) {
+        const toast = await this.toastCtrl.create({
+            message: msg,
+            duration: 3000,
+            position: 'bottom'
+        });
+        toast.present();
+    }
 
 
 
